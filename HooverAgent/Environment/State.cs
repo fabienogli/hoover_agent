6429dfaa339887ad9ca8060bd95c2de5ba0c326e
Irindul <mathieu.regnard2@gmail.com>
2019-01-29 23:56:41 -0500

    Implemented State to store stuff

--- HooverAgent/Environment/State.cs
+++ HooverAgent/Environment/State.cs
@@ -1,7 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Action = HooverAgent.Agent.Action;
+
 namespace HooverAgent.Environment
 {
-    public interface State
+     
+    public class State
     {
-        
+        public Tuple<List<Entities>, Action> Current { get; }
+
+        public Action GetAction()
+        {
+            return Current.Item2;
+        }
+
+        public List<Entities> GetMap()
+        {
+            return Current.Item1;
+        }
+
+        public State(List<Entities> entities, Action action)
+        {
+            Current = new Tuple<List<Entities>, Action>(entities, action);
+        }
     }
 }
