7d519803d783591fe5f17cf48d8e6464a6be8076
Fabien <fabien.ogli@etu.univ-lyon1.fr>
2019-02-01 14:12:54 -0500

    quantification of performace

---
+++ HooverAgent/Environment/Performances.cs
@@ -0,0 +1,11 @@
+namespace HooverAgent.Environment
+{
+    public enum Performances
+    {
+        PickJewel = 10,
+        SniffDust = 4,
+        Move = -1,
+        SniffJewel = -12,
+        PickNothing = -3
+    }
+}
